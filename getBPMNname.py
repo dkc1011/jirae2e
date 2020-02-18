87f50f9d30ef86b3f0d1cb3180b8169389b11aff
Evan Barry <t00202376@students.ittralee.ie>
2020-02-18 12:30:56 +0000

    Modified getBPMNname.py to stop checking for BPMN name when it finds the first instance of the name

--- getBPMNname.py
+++ getBPMNname.py
@@ -1,12 +1,14 @@
 filepath = "diagram_1.bpmn"
 substring = "<bpmn:documentation>Feature: "
 filename = ""
+filenameFound = False
 with open(filepath) as fp:
     line = fp.readline()
     count = 1
-    while line:
+    while line and filenameFound is False:
         if substring in line:
             filename = line.strip()[substring.__len__():]
+            filenameFound = True
         line = fp.readline()
         count += 1
 
