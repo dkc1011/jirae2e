77e878b6a55b8486b137627fa0d0b1dc14076e64
dkc1011 <43175543+dkc1011@users.noreply.github.com>
2020-02-25 09:20:55 +0000

    Changed final value to filename rather than branch

--- APIRequester.py
+++ APIRequester.py
@@ -6,8 +6,36 @@ gitLab = gitlab.Gitlab('https://gitlab.com', private_token='mn4DkrPwSDRXBLWvz_BB
 #Define Project (Proj ID = 17049578) from User (evan_barry ID = 5480838)
 project = gitLab.projects.get(17049578)
 
+def getMergingCommitId(sourceBranch):
+    commits = project.commits.list()
+    for com in commits:
+        if (com.title == "Merge branch '" + sourceBranch + "' into 'master'"):
+            return com.short_id
+
+    return 'No ID found'
+
 #Define Merge Requests that have been merged
 mrs = project.mergerequests.list(state="merged")
+sourceBranch = mrs[0].source_branch
 
-#Print the most recent Merge Request's source branch to Standard-output
-print(mrs[0].source_branch)
+commitId = getMergingCommitId(sourceBranch)
+
+if(commitId != 'No ID found'):
+    commit = project.commits.get(commitId)
+    #first - 4ab347b4
+    #second - 7d6d3893
+
+    masterChanges = commit.diff()
+
+    filename = masterChanges[0]['new_path']
+
+    filenameClipped = filename[:-5]
+
+    print(filenameClipped)
+else:
+    print('')
+
+
+
+
+
